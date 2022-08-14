756211cf409d7f08159dd07d4e20888ead789d31
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-08-14 15:13:41 +0100

    moved on window load script to head

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
-window.addEventListener("load", (event) => {
-  console.log(localStorage.getItem("theme"));
-  if (localStorage.theme == "dark") {
-    change();
-  } else if (localStorage.theme == "light") {
-    localStorage.theme == "light";
-  } else localStorage.removeItem("theme");
-});
+// window.addEventListener("load", (event) => {
+//   console.log(localStorage.getItem("theme"));
+//   if (localStorage.theme == "dark") {
+//     change();
+//   } else if (localStorage.theme == "light") {
+//     localStorage.theme == "light";
+//   } else localStorage.removeItem("theme");
+// });
 
 (function ($) {
   var $window = $(window),
