e2655ed4f17e11fdd76d748045731e813d7ab49d
0ussamabernou <oussama.bernou@outlook.com>
2024-04-28 03:29:57 +0100

    update

--- dist/js/head.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", (event) => {
-  console.log(localStorage.getItem("theme"));
-  if (localStorage.theme == "dark") {
-    change();
-  } else if (localStorage.theme == "light") {
-    localStorage.theme == "light";
-  } else localStorage.removeItem("theme");
-});
